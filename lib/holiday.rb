719a6aef0c94f6bdf2052456325aa3168771e814
Precious Humphrey <phumphrey23@gmail.com>
2018-04-25 03:37:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,6 +61,6 @@ def all_holidays_with_bbq(holiday_hash)
 
     holiday_hash.map do |season, holidays|
     holidays.map {|holiday, supplies|holiday if supplies.include?("BBQ")}
-  end
+  end.flatten.compact
 
 end

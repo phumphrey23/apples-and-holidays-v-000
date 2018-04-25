fe3e61cbd2605562c97f209f18dbe769a620e703
Precious Humphrey <phumphrey23@gmail.com>
2018-04-25 02:37:33 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
   holiday_hash.each do |season, holidays|
-    puts "#{season} : #{holidays}"
+    puts "#{season.capitalize}:"
   end
 end
 
